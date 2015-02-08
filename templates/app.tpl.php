5fd724a964c18b7f0431d7c4ed3a8ce75efdb26c
Jeff Lyon <jeff@albatrossdigital.com>
2015-02-08 00:56:20 -0800

    Making edit, crop work, other feature improvements and bugfixes

--- templates/app.tpl.php
+++ templates/app.tpl.php
@@ -5,20 +5,20 @@
   apiUrl: "<?php print $api_url; ?>",
   fieldName: "<?php print $field_name; ?>",
   files: <?php print drupal_json_encode($files, JSON_HEX_APOS); ?>,
-  multiple: <?php print (int)$multiple; ?>,
+  cardinality: <?php print (int)$cardinality; ?>,
   flickrApiKey: "<?php print $flickr_key; ?>"
 })'><!-- nv-file-drop="" uploader="uploader"-->
     
-    <a href class="thumbnail" ng-repeat="(key, file) in files" title="{{file.name}}">
-      <img ng-src="{{file.thumbUrl}}" />
-      <div class="thumbnail-title">{{file.name}}</div>
-        <button type="button" class="btn btn-default btn-xs thumbnail-edit" ng-click="item.remove()"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></button>
-        <button type="button" class="btn btn-danger btn-xs thumbnail-remove" ng-click="item.remove(key)"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
-    </a>
+    <div  class="thumbnail" ng-repeat="(key, file) in files" title="{{file.name}}">
+      <div thumbnail></div>
+      <a href class="thumbnail-title" ng-click="edit(file.fid)">{{file.name}}</a>
+        <button type="button" class="btn btn-default btn-xs thumbnail-edit" ng-click="edit(file.fid)"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></button>
+        <button type="button" class="btn btn-danger btn-xs thumbnail-remove" ng-click="remove(key)"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
+    </div>
     <div class="thumbnail drop-zone">
       <div><span class="glyphicon glyphicon-download" aria-hidden="true"></span> Drop files</div>
       <div class="or">or</div>
-      <a class="btn btn-primary media-select" ui-sref="modal.upload"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Select</a>
+      <button class="btn btn-primary media-select" ng-click="select($event)"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Select</button>
     </div>
   </div>
 
